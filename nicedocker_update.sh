dbc24f31d89f475c814bc4fdde2c982243766ad1
William Hanwoody <hanwoody@gmail.com>
2014-07-22 15:40:52 +0800

    git to https

--- nicedocker_update.sh
+++ nicedocker_update.sh
@@ -3,7 +3,7 @@
 dd=`date +%s`
 tmpdir=/tmp/nicedocker-$dd
 mkdir $tmpdir
-git clone git@github.com:nicescale/nicedocker.git $tmpdir/
+git clone https://github.com/nicescale/nicedocker.git $tmpdir/
 cd $tmpdir
 sed -i "s/^REPOHOST=nicedocker.com/REPOHOST=127.0.0.1/" nicedocker
 sed -i "/cfagent/d" nicedocker
