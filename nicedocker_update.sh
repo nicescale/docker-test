5ca53a082915888ce86a8979b5753aa0b4c2ccc7
William Hanwoody <hanwoody@gmail.com>
2014-07-25 16:36:32 +0800

    testing clone dev code

--- nicedocker_update.sh
+++ nicedocker_update.sh
@@ -3,7 +3,7 @@
 dd=`date +%s`
 tmpdir=/tmp/nicedocker-$dd
 mkdir $tmpdir
-git clone https://github.com/nicescale/nicedocker.git $tmpdir/
+git clone --branch testing https://github.com/nicescale/nicedocker.git $tmpdir/
 cd $tmpdir
 sed -i "s/^REPOHOST=nicedocker.com/REPOHOST=127.0.0.1/" nicedocker
 sed -i "/cfagent/d" nicedocker
