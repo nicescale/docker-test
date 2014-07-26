86ea1534fbfb1ded8aa48283e78a8e13549d694a
William Hanwoody <hanwoody@gmail.com>
2014-07-26 15:18:25 +0800

    no del cfagent line

--- nicedocker_update.sh
+++ nicedocker_update.sh
@@ -6,7 +6,7 @@ mkdir $tmpdir
 git clone --branch testing https://github.com/nicescale/nicedocker.git $tmpdir/
 cd $tmpdir
 sed -i "s/^REPOHOST=nicedocker.com/REPOHOST=127.0.0.1/" nicedocker
-sed -i "/cfagent/d" nicedocker
+#sed -i "/cfagent/d" nicedocker
 /bin/cp nicedocker /usr/local/bin/nicedocker
 
 cd /tmp
