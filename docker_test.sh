de7a9fadff8911f94ee3c608ed3f6e7c0913af70
William Hanwoody <hanwoody@gmail.com>
2014-07-28 13:28:35 +0800

    fix var

--- docker_test.sh
+++ docker_test.sh
@@ -13,6 +13,6 @@ tags=`get_tags $STACK $BRANCH`
 for i in {1..100}; do
  for t in `echo $tags|tr ',' ' '`; do
   [ "$t" = "latest" ] && continue
-  $PREFIX_DIR/test_image.sh nicescale/$s $t
+  $PREFIX_DIR/test_image.sh nicescale/$STACK $t
  done
 done
