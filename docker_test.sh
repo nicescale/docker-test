f4aac21d3a27c80ef2125c14703b9fbc89ce80f3
William Hanwoody <hanwoody@gmail.com>
2014-07-28 17:01:59 +0800

    test 10

--- docker_test.sh
+++ docker_test.sh
@@ -10,7 +10,7 @@ BRANCH=$2
 
 tags=`get_tags $STACK $BRANCH`
 
-for i in {1..100}; do
+for i in {1..10}; do
  for t in `echo $tags|tr ',' ' '`; do
   [ "$t" = "latest" ] && continue
   $PREFIX_DIR/test_image.sh nicescale/$STACK $t
