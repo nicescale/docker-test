ec88fa2120600cb6f634672ea69a71ff909df930
William Hanwoody <hanwoody@gmail.com>
2014-07-28 20:45:09 +0800

    pull all images of nicescale

--- docker_pull.sh
+++ docker_pull.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-STACK=$1
-BRANCH=$2
-NICEDOCKER_URL=
+NICEDOCKER_URL=repo.nicedocker.com:5000
 PREFIX_DIR=$(dirname `readlink -f $0`)
 . $PREFIX_DIR/get_images.sh
 
-tags=`get_tags $STACK $BRANCH`
-for t in `echo $tags|tr ',' ' '`; do
-  docker pull $NICEDOCKER_URL/nicescale/$STACK:$t
-  docker tag $NICEDOCKER_URL/nicescale/$STACK:$t nicescale/$STACK:$t
+for STACK in $STACKLIST; do
+  for BRANCH in `get_branch $STACK`; do
+    tags=`get_tags $STACK $BRANCH`
+    for t in `echo $tags|tr ',' ' '`; do
+      docker pull $NICEDOCKER_URL/nicescale/$STACK:$t
+      docker tag $NICEDOCKER_URL/nicescale/$STACK:$t nicescale/$STACK:$t
+    done
+  done
 done
