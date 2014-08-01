f81d97d18585f11037f8c7b2a1a432f2ab428cd8
William Hanwoody <hanwoody@gmail.com>
2014-08-01 09:57:37 +0800

    add dockernice link

--- nicedocker_update.sh
+++ nicedocker_update.sh
@@ -8,6 +8,7 @@ cd $tmpdir
 sed -i "s/^REPOHOST=nicedocker.com/REPOHOST=127.0.0.1/" nicedocker
 #sed -i "/cfagent/d" nicedocker
 /bin/cp nicedocker /usr/local/bin/nicedocker
+ln -s nicedocker /usr/local/bin/dockernice
 
 cd /tmp
 rm -fr $tmpdir
