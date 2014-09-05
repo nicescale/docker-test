b9a770d894d33c06df3e98ab4fcc8c077b8349c1
William Hanwoody <hanwoody@gmail.com>
2014-09-05 18:34:38 +0800

    fit new nicedocker

--- test_image.sh
+++ test_image.sh
@@ -15,8 +15,8 @@ die() {
   /bin/echo -ne "Error: $simage:$stag:$sid: " 1>&2
   /bin/echo $* 1>&2
   sleep 2
-  dockernice service $sid stop
-  dockernice service $sid destroy || echo "Error: failed to service $sid destroy!!" 1>&2
+  dockernice stop $sid
+  dockernice destroy $sid || echo "Error: failed to service $sid destroy!!" 1>&2
   exit 1
 }
 error() {
@@ -36,12 +36,12 @@ waits() {
     es=$?
     if [ "$status" = "running" ]; then
       [ $es -eq 0 ] && break
-      dockernice service $sid start
+      dockernice start $sid 
     else
       [ $es -eq 0 ] || break
       initctl status docker-$sid|grep stop &&
       docker kill $sid
-      dockernice service $sid stop
+      dockernice stop $sid 
     fi
     count=$(( count+1 ))
     sleep 1
@@ -65,56 +65,56 @@ iecho exec echo in docker
 dockernice exec $sid echo helloworld || die "failed to exec cmd in docker!!"
 sleep 3
 iecho restart service $sid
-dockernice service $sid restart || die "failed to restart service!!"
+dockernice restart $sid || die "failed to restart service!!"
 waits
 iecho view running dockers and services
 dockernice ps 
 iecho start docker
-dockernice start $sid || die "failed to start docker!!"
+docker start $sid || die "failed to start docker!!"
 waits
 iecho stop docker
-dockernice stop $sid || die "failed to stop docker!!"
+docker stop $sid || die "failed to stop docker!!"
 iecho kill docker
-dockernice kill $sid || die "failed to kill docker!!"
+docker kill $sid || die "failed to kill docker!!"
 waits
 iecho 'test docker autorestart'
-dockernice service $sid status || die "failed to watch docker!!"
+dockernice status $sid || die "failed to watch docker!!"
 iecho start docker 
-dockernice start $sid || die "failed to start docker!!"
+docker start $sid || die "failed to start docker!!"
 iecho start docker again
-dockernice start $sid || die "failed to double start docker!!"
+docker start $sid || die "failed to double start docker!!"
 waits
 iecho reload service
-dockernice service $sid reload || die "failed to reload service!!"
+dockernice reload $sid || die "failed to reload service!!"
 waits
 iecho restart service
-dockernice service $sid restart || die "failed to restart service!!"
+dockernice restart $sid || die "failed to restart service!!"
 waits
 iecho stop service $sid 
-dockernice service $sid stop || die "failed to stop service!!"
+dockernice stop $sid || die "failed to stop service!!"
 waits 5 stopped
 iecho stop service $sid again
-dockernice service $sid stop || die "failed to double stop service!!"
+dockernice stop $sid || die "failed to double stop service!!"
 waits 5 stopped
 iecho status service $sid
-dockernice service $sid status
+dockernice status $sid 
 es=$?
 [ $es -eq 0 ] && die "exit status:$es, service status should return 1!!"
 [ $es -eq 2 -o $es -eq 3 ] && die "exit status:$es, service status should return 1!!"
 
 iecho start service $sid
-dockernice service $sid start || die "failed to start service!!"
+dockernice start $sid || die "failed to start service!!"
 iecho start service $sid again
-dockernice service $sid start || die "failed to double start service!!"
+dockernice start $sid || die "failed to double start service!!"
 waits
 iecho status service $sid
-dockernice service $sid status || die "exit status:$?, service status should return zero!!"
+dockernice status $sid || die "exit status:$?, service status should return zero!!"
 iecho remove service $sid
-dockernice service $sid destroy && die "remove running service should failed!!"
+dockernice destroy $sid && die "remove running service should failed!!"
 iecho remove service $sid again
-dockernice service $sid stop || die "failed to stop service!!"
-dockernice kill $sid
-dockernice service $sid destroy || die "failed to remove service!!"
+dockernice stop $sid || die "failed to stop service!!"
+docker kill $sid
+dockernice destroy $sid || die "failed to remove service!!"
 
 # when exception, then reserve upstart log for troubleshooting
 /bin/rm /var/log/upstart/docker-$sid.log
